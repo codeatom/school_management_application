287afc5395cc70bd9f878881aa76ed7c27560057
Christopher Ojaide <christopher.ojaide@gmail.com>
2022-08-09 05:05:26 +0200

    Initial commit

---
+++ src/test/java/school_management_application/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package school_management_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
